79e790b9d3dd6052893e1955b8f006918abce650
mnforlenza <mnforlenza@gmail.com>
2016-10-20 01:04:32 -0300

    [BUG] Test para bug material

--- src/test/java/org/educa/core/test/security/TestSerializacion.java
+++ src/test/java/org/educa/core/test/security/TestSerializacion.java
@@ -1,8 +1,14 @@
 package org.educa.core.test.security;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.educa.core.entities.model.ExamenUnidad;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -19,4 +25,17 @@ public class TestSerializacion {
 		ExamenUnidad examen = mapper.readValue(json,ExamenUnidad.class);
 		System.out.println(examen);
 	}
+	
+	@Test
+	public void testParserXML() throws JDOMException, IOException{
+		SAXBuilder saxBuilder = new SAXBuilder();
+		Document doc = saxBuilder.build(new StringReader("<p>	<br /> <strong>holass <br /> <br /> </strong></p>"));
+	    String contenidoPuro = doc.getRootElement().getText();
+		contenidoPuro = contenidoPuro.trim();
+	    System.out.println(contenidoPuro);
+	    
+	    assertEquals(contenidoPuro == null || contenidoPuro.isEmpty(),false);
+	}
+	    
+	
 }
