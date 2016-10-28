f817ccb77c4dc83035a6553fbbed7befdf87ff0c
Nestor Huallpa <huallpa.nestor@gmail.com>
2016-10-28 00:52:49 -0300

    agregar tema y listar tema

--- src/main/java/org/educa/core/api/TemaApiController.java
+++ src/main/java/org/educa/core/api/TemaApiController.java
@@ -1,14 +1,18 @@
 package org.educa.core.api;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.educa.core.dao.TemaRepository;
+import org.educa.core.entities.model.Comentario;
 import org.educa.core.entities.model.Tema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,6 +42,23 @@ public class TemaApiController {
 			return new ResponseEntity<List<Tema>>(temas, HttpStatus.NOT_FOUND);
 		}
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "crear")
+	public ResponseEntity<Tema> crear(@RequestBody Tema tema) {
+		
+		Tema resultado = null;
+		
+		Date fechaCreacion = Calendar.getInstance().getTime(); 
+		tema.setFechaCreacion(fechaCreacion);
+		try {
+			resultado = temaRepository.save(tema);
+			return new ResponseEntity<Tema>(resultado, HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<Tema>(resultado, HttpStatus.NOT_FOUND);
+
+		}
 	}
 
 	
